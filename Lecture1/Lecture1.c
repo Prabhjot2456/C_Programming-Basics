1ad2f1ea397912c9600238f976d631b762fdb0e8
Prabhjot2456 <singhprabhjot.prab@gmail.com>
2026-01-22 19:00:03 +0530

    Add files via upload

---
+++ Lecture1/Lecture1.c
@@ -0,0 +1,43 @@
+#include<stdio.h> // lib function 
+// #include // It is a pre processor directive // #if #ifelse #define ...
+// <stdio.h> // Header file
+
+/*
+RT   FxnName(arg/param);  // function declaration 
+void mrg();       // NR  NA   (No return   No Argument)
+void mrg2(int);   // NR  WA
+int mrg3(int);    // WR  WA
+int mrg4();       // WR  NA 
+
+
+mrg();  // function calling
+mrg2(int);
+mrg3(int);
+mrg4();
+
+
+void mrg()  // function definition
+{
+ ...
+}
+
+void mrg2(int a)
+{ 
+}
+
+int mrg3(int a);
+{
+ return 501;
+}
+
+int mrg4()
+{
+return 501;
+}
+*/
+
+int main()
+{
+    printf("Hello World!\n"); // It is function calling because it is an inbuilt function
+    return 0;
+}
